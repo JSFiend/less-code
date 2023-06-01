722c05936dfcca967db2e1375f8d26aa21f1a744
李杰和 <lijiehe@lijiehedeMacBook-Pro.local>
2023-06-01 23:50:02 +0800

    feat: update all module

---
+++ packages/server/src/main.ts
@@ -0,0 +1,9 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  console.log('port', 3000);
+  await app.listen(3000);
+}
+bootstrap();
